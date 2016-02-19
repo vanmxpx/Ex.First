e3a4cdf77511fe463f5b146a48fe22fe3b93beb9
Nikita Tsyhankov <nikita1996.tsyhankov@gmail.com>
2016-02-19 19:46:03 +0200

    v02

--- Ex1Nikita/Ex1Nikita/Program.cs
+++ Ex1Nikita/Ex1Nikita/Program.cs
@@ -16,7 +16,7 @@ namespace Ex1Nikita
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
